0ea39f0b710db4e2792648a7127f3eea8cb71690
Scott Brandley <8163388+DocCaliban@users.noreply.github.com>
2025-12-01 21:48:11 -0800

    quick fix

--- src/utility/file.utilities.ts
+++ src/utility/file.utilities.ts
@@ -12,6 +12,10 @@ export const writePngToFile = async (
   png: PNG,
   path: string
 ): Promise<void> => {
+
+  const dir = path.substring(0, path.lastIndexOf("/")); // extract folder path
+  await fs.mkdir(dir, { recursive: true });             // create folder if missing
+
   const buffer = PNG.sync.write(png);
   await fs.writeFile(path, buffer);
 };
